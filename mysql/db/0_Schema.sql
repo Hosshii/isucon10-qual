4dcab48f9b3ca26ba6c2db992fd5fe1cd62c8f17
Hosshii <sao_heath6147.wistre@icloud.com>
2020-10-01 00:29:03 +0900

    totyuu

---
+++ mysql/db/0_Schema.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS isuumo;
+CREATE DATABASE isuumo;
+
+DROP TABLE IF EXISTS isuumo.estate;
+DROP TABLE IF EXISTS isuumo.chair;
+
+CREATE TABLE isuumo.estate
+(
+    id          INTEGER             NOT NULL PRIMARY KEY,
+    name        VARCHAR(64)         NOT NULL,
+    description VARCHAR(4096)       NOT NULL,
+    thumbnail   VARCHAR(128)        NOT NULL,
+    address     VARCHAR(128)        NOT NULL,
+    latitude    DOUBLE PRECISION    NOT NULL,
+    longitude   DOUBLE PRECISION    NOT NULL,
+    rent        INTEGER             NOT NULL,
+    door_height INTEGER             NOT NULL,
+    door_width  INTEGER             NOT NULL,
+    features    VARCHAR(64)         NOT NULL,
+    popularity  INTEGER             NOT NULL,
+    popularity_desc INTEGER AS (-popularity) NOT NULL,
+    INDEX idx_rent (rent),
+    INDEX idx_latitude (latitude),
+    INDEX idx_longitude(longitude),
+    INDEX idx_popularity_desc (popularity_desc)
+);
+
+CREATE TABLE isuumo.chair
+(
+    id          INTEGER         NOT NULL PRIMARY KEY,
+    name        VARCHAR(64)     NOT NULL,
+    description VARCHAR(4096)   NOT NULL,
+    thumbnail   VARCHAR(128)    NOT NULL,
+    price       INTEGER         NOT NULL,
+    height      INTEGER         NOT NULL,
+    width       INTEGER         NOT NULL,
+    depth       INTEGER         NOT NULL,
+    color       VARCHAR(64)     NOT NULL,
+    features    VARCHAR(64)     NOT NULL,
+    kind        VARCHAR(64)     NOT NULL,
+    popularity  INTEGER         NOT NULL,
+    stock       INTEGER         NOT NULL,
+    popularity_desc INTEGER AS (-popularity) NOT NULL,
+    INDEX idx_price (price),
+    INDEX idx_popularity_desc (popularity_desc)
+);
